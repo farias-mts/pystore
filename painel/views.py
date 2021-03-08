62071775a7816a2975db0bc38caaebbf909c8189
Matheus Farias <farias.mts@outlook.com>
2021-03-08 16:49:29 -0300

    Creating friendly page for admin

--- painel/views.py
+++ painel/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_protect    
 
 # Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        print(request.POST['username'])
+        print(request.POST['password'])
+    return render(request, 'login/login.html')
+
+@login_required(login_url='login/')
+def index(request):
+    return HttpResponse('Logado')
