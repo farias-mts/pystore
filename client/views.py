39cd8555f35c246f4b1de55115ad0766eb12a6cb
Matheus Farias <farias.mts@outlook.com>
2021-03-18 16:13:06 -0300

    New home page create

--- client/views.py
+++ client/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
-from painel.models import Category, Brand
+from painel.models import Category, Brand, Product
 
 def index(request):
     categories = Category.objects.all()
+    products = Product.objects.all()
     context = {
-        'categories':categories
+        'categories':categories,
+        'products':products,
     }
-    return render(request, 'base.html', context)
+    return render(request, 'index.html', context)
 
 def teste(request):
     return render(request, 'pieces/slide.html')
